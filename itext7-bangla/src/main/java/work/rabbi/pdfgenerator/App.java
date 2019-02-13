d88f708b72c12d18a10e3da9274068f55941f938
Rabbi <rabbi.se@gmail.com>
2019-02-13 21:22:40 +0600

    Modified code to generate pdf with bangla text.

--- itext7-bangla/src/main/java/work/rabbi/pdfgenerator/App.java
+++ itext7-bangla/src/main/java/work/rabbi/pdfgenerator/App.java
@@ -21,7 +21,7 @@ public class App
 
         System.out.println( "Hello World!" );
         try {
-            new App().createPdf("test.pdf");
+            new App().createPdf("modified_generated.pdf");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -52,40 +52,40 @@ public class App
 
         // start of bangla test operation
         // bangla font path
-//        String banglaFontPath = "/home/mehmet/IdeaProjects/ITextCDemo/fonts/Bangla.ttf";
-//
-//        // bangla font
-//        PdfFont banglaFont = PdfFontFactory.createFont(banglaFontPath, PdfEncodings.IDENTITY_H, true, true);
-//
-//        // start of bangla test operation
-//        // bangla font path
-//        String banglaFontPath1 = "/home/mehmet/IdeaProjects/ITextCDemo/fonts/SolaimanLipi_20-04-07.ttf";
-//
-//        // bangla font
-//        PdfFont banglaFont1 = PdfFontFactory.createFont(banglaFontPath1, PdfEncodings.IDENTITY_H, true, true);
-//
-//        // bangla text
-////        String text = "\u099A\u09BE\u09AA\u09BE\u0987\u09A8\u09AC\u09BE\u09AC\u0997\u09A8\u09CD\u099C \u09A6\u09A8\u09CD\u09A6\u09CD\u09AC";
-//        String text = " \u09AA\u09CD\u09B0\u09B8\u09BE\u09B0  \u09AE\u09A6\u09CD\u09AF\u09AA\u09BE\u09A8  \u0986\u09AE\u09BF \u0995\u09CB\u09A8 \u09AA\u09A5\u09C7 \u0995\u09CD\u09B7\u09C0\u09B0\u09C7\u09B0 \u09B2\u0995\u09CD\u09B7\u09CD\u09AE\u09C0 \u09B7\u09A8\u09CD\u09A1 \u09AA\u09C1\u09A4\u09C1\u09B2 \u09B0\u09C1\u09AA\u09CB \u0997\u0999\u09CD\u0997\u09BE \u098B\u09B7\u09BF";
-//
-//        // add bangla text
-//        Paragraph banglaPara = new Paragraph(text);
-//
-//        // set bangla font to paragraph
-//        banglaPara.setFont(banglaFont);
-//
-//        // add bangla text
-//        Paragraph banglaPara1 = new Paragraph(text);
-//
-//        // set bangla font to paragraph
-//        banglaPara1.setFont(banglaFont1);
-//
-//        // add bangla paragraph to document
-//        document.add(banglaPara);
-//
-//        // add bangla paragraph to document
-//        document.add(banglaPara1);
-//        // end bangla operation
+        String banglaFontPath = "/home/mehmet/IdeaProjects/ITextCDemo/fonts/Bangla.ttf";
+
+        // bangla font
+        PdfFont banglaFont = PdfFontFactory.createFont(banglaFontPath, PdfEncodings.IDENTITY_H, true, true);
+
+        // start of bangla test operation
+        // bangla font path
+        String banglaFontPath1 = "/home/mehmet/IdeaProjects/ITextCDemo/fonts/SolaimanLipi_20-04-07.ttf";
+
+        // bangla font
+        PdfFont banglaFont1 = PdfFontFactory.createFont(banglaFontPath1, PdfEncodings.IDENTITY_H, true, true);
+
+        // bangla text
+//        String text = "\u099A\u09BE\u09AA\u09BE\u0987\u09A8\u09AC\u09BE\u09AC\u0997\u09A8\u09CD\u099C \u09A6\u09A8\u09CD\u09A6\u09CD\u09AC";
+        String text = " \u09AA\u09CD\u09B0\u09B8\u09BE\u09B0  \u09AE\u09A6\u09CD\u09AF\u09AA\u09BE\u09A8  \u0986\u09AE\u09BF \u0995\u09CB\u09A8 \u09AA\u09A5\u09C7 \u0995\u09CD\u09B7\u09C0\u09B0\u09C7\u09B0 \u09B2\u0995\u09CD\u09B7\u09CD\u09AE\u09C0 \u09B7\u09A8\u09CD\u09A1 \u09AA\u09C1\u09A4\u09C1\u09B2 \u09B0\u09C1\u09AA\u09CB \u0997\u0999\u09CD\u0997\u09BE \u098B\u09B7\u09BF";
+
+        // add bangla text
+        Paragraph banglaPara = new Paragraph(text);
+
+        // set bangla font to paragraph
+        banglaPara.setFont(banglaFont);
+
+        // add bangla text
+        Paragraph banglaPara1 = new Paragraph(text);
+
+        // set bangla font to paragraph
+        banglaPara1.setFont(banglaFont1);
+
+        // add bangla paragraph to document
+        document.add(banglaPara);
+
+        // add bangla paragraph to document
+        document.add(banglaPara1);
+        // end bangla operation
 
 
         //Close document
