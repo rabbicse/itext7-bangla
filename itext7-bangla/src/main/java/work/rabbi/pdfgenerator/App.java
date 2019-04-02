9a4dfea288407b7695989f50d7987322bed93dee
Rabbi <rabbi.se@gmail.com>
2019-04-02 20:40:09 +0600

    Modified code to check more Bangla font.

--- itext7-bangla/src/main/java/work/rabbi/pdfgenerator/App.java
+++ itext7-bangla/src/main/java/work/rabbi/pdfgenerator/App.java
@@ -10,21 +10,24 @@ import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Paragraph;
 
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    private static final String BANGLA_TEXT = "\u09AA\u09CD\u09B0\u09B8\u09BE\u09B0  \u09AE\u09A6\u09CD\u09AF\u09AA\u09BE\u09A8  \u0986\u09AE\u09BF \u0995\u09CB\u09A8 \u09AA\u09A5\u09C7 \u0995\u09CD\u09B7\u09C0\u09B0\u09C7\u09B0 \u09B2\u0995\u09CD\u09B7\u09CD\u09AE\u09C0 \u09B7\u09A8\u09CD\u09A1 \u09AA\u09C1\u09A4\u09C1\u09B2 \u09B0\u09C1\u09AA\u09CB \u0997\u0999\u09CD\u0997\u09BE \u098B\u09B7\u09BF";
+    private static final String BANGLA_TTF_PATH = "fonts/Bangla.ttf";
 
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+
+        System.out.println("Trying to create PDF...");
         try {
             new App().createPdf("modified_generated.pdf");
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            System.out.println("PDF generation Done!");
         }
     }
 
@@ -41,41 +44,31 @@ public class App
         // Initialize document
         Document document = new Document(pdf);
 
-        // Create a PdfFont
-        PdfFont font = PdfFontFactory.createFont(StandardFonts.TIMES_ROMAN);
-
-        Paragraph para1 = new Paragraph("Hello World!");
-        para1.setFont(font);
-
         //Add paragraph to the document
-        document.add(para1);
+        document.add(createHeader("Font: Bangla.ttf"));
 
-        // start of bangla test operation
-        // bangla font path
-        String banglaFontPath = getClass().getClassLoader().getResource("fonts/Bangla.ttf").getPath(); //"/home/mehmet/IdeaProjects/ITextCDemo/fonts/Bangla.ttf";
+        // Add bangla text to the body
+        document.add(createBody(BANGLA_TEXT, createItextFont(getResourceFontPath(BANGLA_TTF_PATH))));
 
         // bangla font
-        PdfFont banglaFont = PdfFontFactory.createFont(banglaFontPath, PdfEncodings.IDENTITY_H, true, true);
+//        PdfFont banglaFont =  createItextFont(getResourceFontPath(BANGLA_TTF_PATH)); //PdfFontFactory.createFont(getResourceFontPath("fonts/Bangla.ttf"), PdfEncodings.IDENTITY_H, true, true);
 
         // start of bangla test operation
         // bangla font path
-        String banglaFontPath1 =  getClass().getClassLoader().getResource("fonts/SolaimanLipi_20-04-07.ttf").getPath();//"/home/mehmet/IdeaProjects/ITextCDemo/fonts/SolaimanLipi_20-04-07.ttf";
+        String banglaFontPath1 = Objects.requireNonNull(getClass().getClassLoader().getResource("fonts/SolaimanLipi_20-04-07.ttf")).getPath();//"/home/mehmet/IdeaProjects/ITextCDemo/fonts/SolaimanLipi_20-04-07.ttf";
 
         // bangla font
         PdfFont banglaFont1 = PdfFontFactory.createFont(banglaFontPath1, PdfEncodings.IDENTITY_H, true, true);
 
-        // bangla text
-//        String text = "\u099A\u09BE\u09AA\u09BE\u0987\u09A8\u09AC\u09BE\u09AC\u0997\u09A8\u09CD\u099C \u09A6\u09A8\u09CD\u09A6\u09CD\u09AC";
-        String text = " \u09AA\u09CD\u09B0\u09B8\u09BE\u09B0  \u09AE\u09A6\u09CD\u09AF\u09AA\u09BE\u09A8  \u0986\u09AE\u09BF \u0995\u09CB\u09A8 \u09AA\u09A5\u09C7 \u0995\u09CD\u09B7\u09C0\u09B0\u09C7\u09B0 \u09B2\u0995\u09CD\u09B7\u09CD\u09AE\u09C0 \u09B7\u09A8\u09CD\u09A1 \u09AA\u09C1\u09A4\u09C1\u09B2 \u09B0\u09C1\u09AA\u09CB \u0997\u0999\u09CD\u0997\u09BE \u098B\u09B7\u09BF";
 
-        // add bangla text
-        Paragraph banglaPara = new Paragraph(text);
+        // add bangla BANGLA_TEXT
+        Paragraph banglaPara = new Paragraph(BANGLA_TEXT);
 
         // set bangla font to paragraph
-        banglaPara.setFont(banglaFont);
+//        banglaPara.setFont(banglaFont);
 
-        // add bangla text
-        Paragraph banglaPara1 = new Paragraph(text);
+        // add bangla BANGLA_TEXT
+        Paragraph banglaPara1 = new Paragraph(BANGLA_TEXT);
 
         // set bangla font to paragraph
         banglaPara1.setFont(banglaFont1);
@@ -94,5 +87,25 @@ public class App
         long endTime = System.currentTimeMillis();
         System.out.println("Elapsed Time: " + (((double) endTime - (double) startTime) / (double) (1000 * 60)) + " Min");
 
+    }
+
+    private Paragraph createHeader(String text) throws IOException {
+        // Create a PdfFont
+        PdfFont font = PdfFontFactory.createFont(StandardFonts.HELVETICA_BOLD);
+
+        return new Paragraph(text).setFont(font);
+    }
+
+    private Paragraph createBody(String text, PdfFont font) {
+
+        return new Paragraph(text).setFont(font);
+    }
+
+    private String getResourceFontPath(String path){
+        return Objects.requireNonNull(getClass().getClassLoader().getResource(path)).getPath();
+    }
+
+    private PdfFont createItextFont(String fontPath) throws IOException {
+        return PdfFontFactory.createFont(fontPath, PdfEncodings.IDENTITY_H, true, true);
     }
 }
